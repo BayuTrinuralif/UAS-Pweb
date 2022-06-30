3a0e1e4c89018447b6bdf2b58462c4a97670e741
BayuTrinuralif <12050110492@students.uin-suska.ac.id>
2022-06-30 15:04:07 +0700

    commit

---
+++ employee mgmt PHP/elms/admin/includes/sidebar.php
@@ -0,0 +1,75 @@
+     <aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/foto.jpg" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                       
+                                <p>Admin Bayu</p>
+
+                         
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><i class="material-icons">settings_input_svideo</i>Dashboard</a></li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">apps</i>Departemen<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="adddepartment.php">Tambahkan Departemen</a></li>
+                                <li><a href="managedepartments.php">Kelola Departemen</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">code</i>Jenis Cuti<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addleavetype.php">Tambahkan Jenis Cuti</a></li>
+                                <li><a href="manageleavetype.php">Kelola Jenis Cuti</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>Karyawan<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addemployee.php">Tambahkan Karyawan</a></li>
+                                <li><a href="manageemployee.php">Kelola Karyawan</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+   <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Manajemen Cuti<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="leaves.php">Total Cuti </a></li>
+                                <li><a href="pending-leavehistory.php">Cuti Yang Tertunda </a></li>
+                                <li><a href="approvedleave-history.php">Cuti Yang Disetujui</a></li>
+                                  <li><a href="notapproved-leaves.php">Cuti Yang Tidak Disetujui</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+
+
+
+                        <li class="no-padding">
+                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Sign Out</a>
+                            </li>  
+                 
+
+                 
+              
+                </ul>
+                   <div class="footer">
+                    <p class="copyright">Dipersembahkan Oleh<a href="http://www.code-projects.org/">Code-Projects </a>©</p>
+                
+                </div>
+                </div>
+            </aside>
